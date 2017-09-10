c330bc8a8dda72a915f73331946f6c8ac69dd8fd
ljorgens <leejorgensen22@gmail.com>
2017-09-10 13:17:31 -0700

    change indentation

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ def hello():
 	return render_template('hello.html',**locals())
  
 if __name__ == "__main__":
-	  port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
